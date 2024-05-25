38bf677e4fed233386d16275fda36049e6330d27
sharayu-potuwar <sharayu.potuwar@gmail.com>
2024-05-24 22:53:13 -0400

    refactor db connection

--- app/functions.py
+++ app/functions.py
@@ -2,35 +2,22 @@ import logging
 import boto3
 import psycopg2
 from botocore.exceptions import ClientError
-from app.utils import secrets, sns
+from app.utils import secrets, sns, db_connection
 from app.config import config
 
 secret = secrets.Secret()
 sns = sns.Sns()
+db_connection = db_connection.DbConnection()
 
 class registration:
     # SAVE THE DB CONFIG IN A DICT OBJECT
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.DATABASE_CONFIG = {
-            "database": "ecommerce",
-            "user": "ecom_user",
-            "password": secret.get_secret(secret_name=config.DB_SECRET_NAME),
-            "host": config.DB_HOST,
-            "port": 5432,
-        }
-
-        self.conn = psycopg2.connect(
-            dbname=self.DATABASE_CONFIG.get("database"),
-            user=self.DATABASE_CONFIG.get("user"),
-            password=self.DATABASE_CONFIG.get("password"),
-            host=self.DATABASE_CONFIG.get("host"),
-            port=self.DATABASE_CONFIG.get("port"),
-        )
 
     def insert_customer(self, dicobj):
         self.logger.info("insert_customer begin.. ")
-        curr = self.conn.cursor()
+        conn = db_connection.get_connection()
+        curr = conn.cursor()
 
         # EXECUTE THE INSERT QUERY
         curr.execute(
@@ -54,15 +41,17 @@ class registration:
             msg = "registration is successfull"
 
         # COMMIT THE ABOVE REQUESTS
-        self.conn.commit()
+        conn.commit()
         # CLOSE THE CONNECTION
-        self.conn.close()
+        conn.close()
         sns.publish_to_sns(topic_arn=config.SNS_TOPIC_ARN,message=msg)
         return msg
 
     def verify_registration(self, email_id):
         self.logger.info("verify_registration begin.. ")
-        curr = self.conn.cursor()
+        conn = db_connection.get_connection()
+        curr = conn.cursor()
+
         curr.execute(
             f"""
             SELECT email_id FROM customer.cust_registration where
@@ -76,10 +65,10 @@ class registration:
         else:
             msg = "User exist.You may login"
         # COMMIT THE ABOVE REQUESTS
-        self.conn.commit()
+        conn.commit()
 
         # CLOSE THE CONNECTION
-        self.conn.close()
+        conn.close()
 
         return msg
 
